ca93ddde04ebc0cdb03ccbae9d079658e19ef5a6
Oleksii Valuiskyi <ovalu@softserveinc.com>
2021-09-03 19:41:03 +0300

    Refactor RegionsController

--- src/main/java/top/jalva/project/controller/RegionsController.java
+++ src/main/java/top/jalva/project/controller/RegionsController.java
@@ -1,36 +1,33 @@
 package top.jalva.project.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import top.jalva.project.domain.Region;
+import top.jalva.project.enums.RegionId;
 import top.jalva.project.repo.RegionsRepo;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
+@RestController("/regions")
+@RequiredArgsConstructor
 public class RegionsController {
 
     RegionsRepo repo;
 
-    @Autowired
-    public RegionsController(RegionsRepo repo) {
-        this.repo = repo;
+    @GetMapping("/")
+    public ResponseEntity<List<RegionId>> getRegionsIds() {
+        List<RegionId> regions = Arrays.stream(RegionId.values()).collect(Collectors.toList());
+        return new ResponseEntity<>(regions, HttpStatus.OK);
     }
 
-    @GetMapping("/regions")
-     ResponseEntity<List<top.jalva.project.enums.Region>> regions() {
-        List<top.jalva.project.enums.Region> regions = Arrays.asList(top.jalva.project.enums.Region.values()).stream().collect(Collectors.toList());
-        return new ResponseEntity(regions, HttpStatus.OK);
-    }
-
-    @GetMapping("/regions/{id}")
-    ResponseEntity<Region> region(@PathVariable String id) {
-        return new ResponseEntity(repo.findById(id), HttpStatus.OK);
+    @GetMapping("/{id}")
+    public ResponseEntity<Region> getRegionById(@PathVariable String id) {
+        return new ResponseEntity<>(repo.findById(id).orElseThrow(), HttpStatus.OK);
     }
 }
